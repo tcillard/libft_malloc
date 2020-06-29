dc1eeef18b7423dc082d29b4e4031e3d7a3556be
tcillard <tcillard@student.42.fr>
2020-06-29 20:23:23 +0200

    add, libft_malloc_struct, add malloc function base

--- include/libft_malloc.h
+++ include/libft_malloc.h
@@ -5,4 +5,13 @@
 # include <stdlib.h>
 # define TINY   100
 # define SMALL  10000
+
+typedef struct s_libft_malloc
+{
+    void    *tiny;
+    void    *small;
+    int     tinyActualSize;
+    int     smallActualSize;
+}             t_libft_malloc;
+
 #endif
